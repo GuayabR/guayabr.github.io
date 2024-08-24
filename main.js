e8038a549ea21d2e8cbe54b08d478fdc9c006fb0
GuayabR <160454807+GuayabR@users.noreply.github.com>
2024-08-24 23:18:26 +1200

    test

---
+++ main.js
@@ -0,0 +1,9 @@
+// script.js
+
+// Initialize AOS library for scroll animations
+document.addEventListener("DOMContentLoaded", function () {
+    AOS.init({
+        duration: 1000, // Animation duration in milliseconds
+        once: true // Whether animation should happen only once - while scrolling down
+    });
+});
